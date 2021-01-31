87d8df51416e979f60999bbaccad95e1015d9123
Icko92 <denkovski.ivica.92@gmail.com>
2021-01-31 23:36:04 +0100

    finished mockup

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -8,9 +8,26 @@ function Footer() {
       <div className="footer-logo">
         <img src={logo} alt="logo" />
       </div>
-      <div className="location-info"></div>
-      <div className="hours-info"></div>
-      <div className="social"></div>
+      <div className="location-info">
+        <h1>Our Location</h1>
+        <p>gfghfhgfhgfhgfh hghhgh</p>
+        <p>gfghfhgfhgfhgfh hghhgh</p>
+      </div>
+      <div className="hours-info">
+        <h1>Hours</h1>
+        <p>gfghfhgfhgfhgfh hghhgh</p>
+        <p>gfghfhgfhgfhgfh hghhgh</p>
+      </div>
+      <div className="social">
+        <h1>Follow Us</h1>
+        <div className="social-logo">
+          <img src={logo} alt="logo" />
+          <img src={logo} alt="logo" />
+          <img src={logo} alt="logo" />
+          <img src={logo} alt="logo" />
+          <img src={logo} alt="logo" />
+        </div>
+      </div>
     </div>
   );
 }
